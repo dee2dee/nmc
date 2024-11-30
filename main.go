94b608703e434d018a7b1bb627440e050ac36da5
dede apriansah <dedeeapr17@gmail.com>
2024-11-30 16:23:33 +0700

    remove old file on main.go

--- main.go
+++ main.go
@@ -7,13 +7,11 @@ import (
 )
 
 func main() {
-	// gin.SetMode(gin.DebugMode)
 
 	models.ConnectDatabase()
 
 	r := routers.SetupRouter()
 
-	// Start the server
 	if err := r.Run(":8080"); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
